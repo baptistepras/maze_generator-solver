d90c2c512645e920bd3e36845728cd6ad51da896
Raphael <rapha@LAPTOP-0QQGUB9V>
2023-11-23 15:02:00 +0100

    J'ai ajouté une fonction qui prend un nom de fichier en paramètres, lit ce fichier et renvoie une liste contenant chaque ligne sous forme de string.

--- maze.ml
+++ maze.ml
@@ -1,11 +1,25 @@
-let () = Printf.printf"Begin..\n"
-
-let lire_fichier chemin = 
-  let fichier = open_in chemin in
-  fichier
-
-let file = lire_fichier "test/maze_2x1.laby"
+let () = Printf.printf"Begin...\n"
 
 
+let path = "test/maze_4x8.laby"
 
-let () = Printf.printf"End..\n"
+
+let load_file file_path =
+  let in_c = open_in file_path in
+ 
+  let rec aux file =
+    try
+      let line = input_line file in
+      line::aux file
+    with
+      End_of_file -> []  in
+  aux in_c
+
+let print_list liste = List.iter (fun x -> Printf.printf"%s\n" x) liste
+
+let file = load_file path
+
+let () =print_list file
+
+
+let fin = Printf.printf"End...\n"
