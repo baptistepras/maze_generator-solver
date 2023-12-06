94fbf45950eb01b3bc70d58b954e57d08496d52e
Raphael Leonardi <rleonar@a-999904.ups.u-psud.fr>
2023-12-06 16:10:45 +0100

    ça marche pas

--- maze.ml
+++ maze.ml
@@ -9,107 +9,100 @@ type labyrinthe = {
   arrivee : int*int;
 }
 
-
-
-let path = "test/maze_100x100.laby"
+let path = "test/maze_4x8.laby"
 
 let suite l = 
   match l with 
   | []    -> []
   | e::ll -> ll 
 
-
-
+let string_to_char_list (s : string) : char list =
+  s |> String.to_seq |> List.of_seq
+  
+ 
 let load_file file_path =
   let in_c = open_in file_path in
  
   let rec aux file =
     try
-      let line = input_line file in
+      let line = string_to_char_list(input_line file) in
       line::aux file
     with
       End_of_file -> []  in
   aux in_c
 
 let transforme c = 
+  
   match c with 
-  | '+' | '-' | '|' -> 0
   | ' ' -> 1
   | 'E' -> 2
   | 'S' -> 3
+  | '+' | '-' | '|' -> 0
   | _   -> failwith"Caractère %s interdit" c
 
-let constructeur file_name = 
-  let file = load_file file_name in 
+
+
+let rec transforme_ligne ligne taille indice tableau = 
+  
+  if taille < 0 then failwith"Fichier non valide" else
+
+  match ligne with 
+  | c::ll ->
+    let b = transforme(c) in
+    
+    tableau.(indice) <- b;
+    transforme_ligne ll (taille - 1) (indice + 1 ) tableau
+  | [] -> ()
+  
+let print_ints list_int = Array.iter (fun x -> Printf.printf"%d" x) list_int; Printf.printf"\n"
+
+let print_tab tab = Array.iter print_ints tab
+
+
+
+
+
+
+
+let rec aux tableau indice_ligne file largeur hauteur = 
+  if indice_ligne > hauteur then failwith"Fichier non valide" else
+
+  match file with 
+  | e::ll ->  transforme_ligne e largeur 0 tableau.(indice_ligne); 
+  aux tableau (indice_ligne + 1 ) ll largeur hauteur
+  | [] -> () 
+  
+let print_line list_char = List.iter (fun c -> Printf.printf"%c" c) list_char
+
+let print_file list_list_char = List.iter print_line list_list_char
+  
+
+let constructeur  file_name = 
+  (*let file = List.map (fun str -> Array.of_list (String.split_on_char ' ' str)) (load_file file_name) in
+  *)
+  let file = load_file file_name in
+  
   let depart = 0  in
   let arrivee = 0 in
 
   (* On définit la taille du labyrinthe*)
-  let hauteur = ((List.length file) -1)/2 in
+  let hauteur = List.length file in
   let largeur =
   match file with 
-  | e::ll ->  ((String.length e) -1)/2 
+  | e::ll ->  List.length e;
   | _     -> failwith"Y  a rien dans le labyrinthe"   in
   
-  let tab = Array.make hauteur (Array.make largeur 1) in
-
-  let transforme_ligne ligne taille numero tableau = 
-    (* Prend en argument une ligne string, elle doit avoir une longueur de taille*)
-    (* Si numero est paire, le début et la fin doit être un +      *)
-    (* Si numero est impaire  le debut et la fin  doit *)
-
-
-    if (((String.length ligne) -1) / 2) <> taille then  failwith"Ligne pas de la bonne taille" 
-    else
-    let rec parcours_ligne ligne indice last_element numero tableau =
-      let caractere = ligne.[indice] in
-      
-      
-      if indice = 2*taille || indice = 0 then begin
-         (*si on est au dernier élément de la liste*)
-        if numero mod 2 = 0 then begin  (* si la ligne est paire*)
-          
-          if caractere <> '+'
-            then begin
-              failwith"Mauvais dernier element" end
-        end else
-          if caractere <> '|' then begin
-            failwith"Mauvais dernier element" end
-
-        tableau.[numero].[indice] = 0
-      end else
-        if caractere <> ' '  then begin
-
-          if caractere = 'E' then begin 
-            if depart = 1 then begin failwith"Plusieurs entrees"
-            end else depart = 1
-          end else
-            if caractere = 'S' then begin
-              if arrivee = 1 then begin failwith"Plusieurs sorties"
-              end else arrivee = 1
-            end else
-              if caractere = last_element then begin
-                failwith"Deux caracteres egaux qui se suivent"
-              end else
-                tableau.[numero].[indice] = transforme caractere
-        end else parcours_ligne ligne (indice+1) caractere numero tableau
-            
-    in
-    parcours_ligne ligne 0 ' ' numero tableau
-    transforme_ligne (suite file) largeur (numero+1) tableau
-  in 
-  transforme_ligne file largeur 0 tab 
-    
-      
-          
-
-  
   
   
 
+  let tab = Array.make_matrix hauteur largeur 0  in
 
- 
+  aux tab 0 file largeur hauteur;
+  tab
+  
+  
 
+  
 
 
   
@@ -118,7 +111,7 @@ let constructeur file_name =
 
 
 let print_line line =
-  String.iter (fun c -> let s = transforme c in Printf.printf"%d " s) line;
+  List.iter (fun c -> let s = transforme c in Printf.printf"%d " s) line;
   Printf.printf"\n"
   
 let print_list liste = List.iter (fun x -> Printf.printf"%s\n" x) liste
@@ -126,6 +119,8 @@ let print_list liste = List.iter (fun x -> Printf.printf"%s\n" x) liste
 let print_walls liste = List.iter print_line liste
 let file = load_file path
 
-let () = constructeur path
+let a = constructeur path
+
+let () = print_tab a
 
 let fin = Printf.printf"End...\n"
