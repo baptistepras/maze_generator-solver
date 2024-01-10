003fcad44bbd3ec168dc018a444d104f060573ad
Raphael Leonardi <raphael.leonardi@universite-paris-saclay.fr>
2024-01-10 20:35:43 +0000

    Replace maze.ml

--- maze.ml
+++ maze.ml
@@ -60,55 +60,8 @@ let verifie_caractere c numero_ligne indice ligne_max indice_max =
         | 1 , 1 ->  c == 'S' || c == 'E' || c == ' '
         | _ , _ -> failwith"Ya un probleme avec le modulo" (*ce cas ne sera jamais atteint, c'est juste pour eviter les warnings*) 
 
-let printMur numero_ligne indice = 
-  
-  match numero_ligne mod 2 , indice mod 2 with
-  | 0 , 0       -> Printf.printf"+"
-  | 0 , 1       -> Printf.printf"-"
-  | 1 , 0 | 1, 1-> Printf.printf"|"
-  | _, _ -> failwith"Probleme avec le modulo" (* cette ligne ne se fait jamais, c'est pur eviter les warnings du pattern matching *)
-
-let printCaractere (layout: int array array) numero_ligne indice = 
-  let i1 = layout.(numero_ligne).(indice) in
-  match i1 with 
-  | 0 -> printMur numero_ligne indice
-  | 1 -> Printf.printf" "
-  | 2 -> Printf.printf"E"
-  | 3 -> Printf.printf"S"
-  | _ -> failwith"Valeur interdite dans le layout"
-
-let  printLigne (layout : int array array) numero_ligne indice_max = 
-  let rec sousPrintLigne (layout : int array array) numero_ligne indice indice_max =
-    if indice > indice_max then (
-      Printf.printf"\n"
-    )
-    else(
-      printCaractere layout numero_ligne indice;
-      sousPrintLigne layout numero_ligne (indice + 1 ) indice_max ;) in
-      
-  sousPrintLigne layout numero_ligne 0 indice_max
-
-let rec sousAffiche layout numero_ligne ligne_max indice_max = 
-  if numero_ligne > ligne_max then(
-    Printf.printf"\n"
-  )else(
-    printLigne layout numero_ligne indice_max;
-    sousAffiche layout (numero_ligne + 1) ligne_max indice_max
-  )
-
-let afficheLabyrinthe labyrinthe = 
-  let indice_max = labyrinthe.murs_largeur - 1 in
-  let ligne_max = labyrinthe.murs_hauteur - 1 in
-  sousAffiche labyrinthe.murs 0 ligne_max indice_max;
-
-  ()
 
 
-let print_2d_array arr =
-  Array.iter (fun row ->
-    Array.iter (fun elem -> print_int elem; print_string " ") row;
-    print_newline ()
-  ) arr
 
 let rec parcours_ligne ligne numero_ligne array indice indice_max= 
     
@@ -197,11 +150,7 @@ let constructeur file_name =
   depart = depart;
   arrivee = arrivee }
 
-let print_line line =
-  String.iter (fun c -> let s = transforme c in Printf.printf"%d " s) line;
-  Printf.printf"\n"
-  
-let print_list liste = List.iter (fun x -> Printf.printf"%s\n" x) liste
+
 let file = load_file path
 
 
@@ -247,14 +196,7 @@ let genereLabyVide hauteur largeur depart arrivee=
   } 
  
 
-let printLaby labyrinthe = 
-  Printf.printf "Hauteur : %d\n" labyrinthe.hauteur; 
-  Printf.printf "Largeur : %d\n" labyrinthe.largeur;
-  Printf.printf "Largeur des murs : %d\n" labyrinthe.murs_largeur;
-  Printf.printf "Hauteur des murs : %d\n" labyrinthe.murs_hauteur;
-  Printf.printf "Plan des murs : \n";
-  print_2d_array labyrinthe.murs 
-  
+
 
 
 let intDirection n =
@@ -299,21 +241,11 @@ let directionsPossibles (visitees:bool array array) numero_ligne indice ligne_ma
 
 
 
-let printDirection d = 
-  match d with 
-  | N -> Printf.printf"N "
-  | E -> Printf.printf"E "
-  | O -> Printf.printf"O "
-  | S -> Printf.printf"S "
 
 
 
 
 
-
-let () = Random.self_init  () 
-
-let printDirections (directions : direction list) = List.iter printDirection directions
 
 
 let rec generate_unique_random_numbers count range_max acc =
@@ -328,7 +260,6 @@ let rec generate_unique_random_numbers count range_max acc =
 let random_numbers_without_repetition count range_max =
   generate_unique_random_numbers count range_max []
 
-let print_list_int liste = List.iter print_int liste
 
 
 
@@ -366,9 +297,9 @@ let rec sousGenereLaby layout numero_ligne indice (visitees : bool array array) 
       
       let new_pos = movePosition numero_ligne indice direction in 
       
-      let _ = Printf.printf"Old pos (%d %d) New pos (%d %d) " numero_ligne indice (fst new_pos) (snd new_pos)  in
+      (* let _ = Printf.printf"Old pos (%d %d) New pos (%d %d) " numero_ligne indice (fst new_pos) (snd new_pos)  in
       let _ = printDirection direction in
-      let _ = Printf.printf"\n" in
+      let _ = Printf.printf"\n" in *)
 
       sousGenereLaby layout (fst new_pos ) (snd new_pos) visitees (numero_ligne, indice) indice_max ligne_max;
 
@@ -428,8 +359,7 @@ let genereLabyrinthe hauteur largeur =
   laby
 
  
-
-let labyrinthe1 = constructeur path
+(* let labyrinthe1 = constructeur path
 let () = Printf.printf"\n"
 let () = printLaby labyrinthe1
 let () = Printf.printf"\n"
@@ -441,12 +371,13 @@ let () = ouvrirChemin a.murs 7 7 N
 let () = afficheLabyrinthe a
  *)
 
-let a = genereLabyrinthe 15 50
+let a = genereLabyrinthe 18 65
 (* let a = genereLabyVide 20 40
  *)let () = Printf.printf"\n" 
 
 
-let () = afficheLabyrinthe a 
+ let () = afficheLabyrinthe a  
 
 
 let fin = Printf.printf"End\n"
+ *)
